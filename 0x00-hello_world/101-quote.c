695a6cf74a96bf30effe5927b6360c34741f18e5
Damilola <jdamilola96@gmail.com>
2023-07-14 07:30:43 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entery point
+ * Return: Always zero success
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (0);
+}
