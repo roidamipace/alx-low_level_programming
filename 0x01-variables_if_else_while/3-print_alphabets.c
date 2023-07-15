b902eeb58a4ec8793397699adaa80ef914d0eb23
Damilola <jdamilola96@gmail.com>
2023-07-15 05:39:37 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 /**
-  * main - Entry point
+  * main - Entery point
   * Description: 'print alphabets lowercase and uppercase'
-  * Return: Always zero
+  * Return: Always 0
   */
 int main(void)
 {
-        int n = 97;
+	int n = 97;
 	int m = 65;
 
-        while (n <= 122)
-        {
-                putchar(n);
-                n++;
-        }
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
 	while (m <= 90)
 	{
 		putchar(m);
 		m++;
 	}
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
