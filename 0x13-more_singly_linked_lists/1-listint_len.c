17376c14157fb1e25a7513d0ada616df35b1d4ef
Damilola <jdamilola96@gmail.com>
2023-08-29 08:49:14 -0700

    Add node

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,10 +3,9 @@
 /**
  * listint_len - returns the number of elements in a linked lists
  * @h: linked list of type listint_t to traverse
- * @next: points to the next node
  *
  * Return: number of nodes
- * 
+ *
  */
 size_t listint_len(const listint_t *h)
 {
--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,13 +1,24 @@
+#include "lists.h"
+
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
+ * add_nodeint - adds a new node at the beginning of a linked list
+ * @head: pointer to the first node in the list
+ * @n: data to insert in the new node
  *
- * Description: singly linked list node structure
- * 
+ * Return: pointer to new node, or NULL if it fails
+ *
  */
-typedef struct listint_s
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-    int n;
-    struct listint_s *next;
-} listint_t;
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
